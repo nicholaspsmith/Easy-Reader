2b2724efc9ca949fb407d7c165460c6c03cf788a
Nick P Smith <npsmith1990@gmail.com>
2015-08-01 16:34:51 -0500

    use vanilla js instead of javascript

--- link_gatherer.js
+++ link_gatherer.js
@@ -1,27 +1,42 @@
-$(document).ready(function(){
+docReady(function(){
+    document.getElementsByTagName('article')[0].innerHTML += '<div class="link-collection"><h3>Links from article:</h3></div>';
+    var lc = document.getElementsByClassName('link-collection')[0];
+    lc.style.textAlign = 'center';
+    lc.style.margin = '30px 0 100px';
 
 
-    $('article').append('<div class="link-collection"><h3>Links from article:</h3></div>');
-    $('.link-collection').css({
-      'text-align': 'center',
-      'margin': '30px 0 100px'
-    });
-    $('article p a').each(function(a,b){
-      $(b).css({
-        'border': 'none',
-        'box-shadow': 'none'
+    var p = document.querySelectorAll('article p')[0];
+    var pColor = getComputedStyle(p)['color'];
+    var css1='article p a{color:' + pColor + ';}';
+    var css2='article p a:hover{text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}';
+    var css3='article p a:active{text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}';
+    var css4='article p a:focus{text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}';
+    style=document.createElement('style');
+    if (style.styleSheet) {
+      style.styleSheet.cssText=css1;
+      style.styleSheet.cssText=css2;
+      style.styleSheet.cssText=css3;
+      style.styleSheet.cssText=css4;
+    }
+    else {
+      style.appendChild(document.createTextNode(css1));
+      style.appendChild(document.createTextNode(css2));
+      style.appendChild(document.createTextNode(css3));
+      style.appendChild(document.createTextNode(css4));
+    }
+    document.getElementsByTagName('head')[0].appendChild(style);
+
+    var ars = document.querySelectorAll('article p a');
+    for (var i = 0; i < ars.length; i++) {
+      ars[i].style.border = 'none';
+      ars[i].style.boxShadow = 'none';
+      var href = ars[i].href;
+      var text = ars[i].text;
+      var anchor = '<div><a href="' + href + '" target="_blank">' + text + '</a></div>';
+      lc.innerHTML += anchor;
+      ars[i].addEventListener('click', function (e){
+        e.preventDefault();
       });
-      var pcolor = $('article p').css('color');
-      $('article p a:hover').css('text-decoration','none');
-      $('article p a:hover').css('color',pcolor);
-      $(b).css('color', pcolor)
-      var href = $(b).attr('href');
-      var text = $(b).html();
-      var anchor = '<a href="' + href + '" target="_blank">' + text + '</a>';
-      $('.link-collection').append('<div>' + anchor + '</div>');
-    });
-    $('article p a').click(function(e){
-      e.preventDefault();
-    });
+    }
 
 });
