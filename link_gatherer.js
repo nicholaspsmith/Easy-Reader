bc5f493de081720de325dc35f806174e1d187b83
Nick P Smith <npsmith1990@gmail.com>
2015-08-01 17:33:05 -0500

    Styled it to make links more accessible

--- link_gatherer.js
+++ link_gatherer.js
@@ -1,28 +1,24 @@
 docReady(function(){
-    document.getElementsByTagName('article')[0].innerHTML += '<div class="link-collection"><h3>Links from article:</h3></div>';
+    document.getElementsByTagName('article')[0].innerHTML += '<div class="link-collection"><h3>Collected Links</h3></div>';
     var lc = document.getElementsByClassName('link-collection')[0];
     lc.style.textAlign = 'center';
-    lc.style.margin = '30px 0 100px';
+    lc.style.margin = '30px 0 0';
 
 
     var p = document.querySelectorAll('article p')[0];
     var pColor = getComputedStyle(p)['color'];
     var css1='article p a{color:' + pColor + ';}';
-    var css2='article p a:hover{text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}';
-    var css3='article p a:active{text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}';
-    var css4='article p a:focus{text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}';
+    var css2='article p a:hover {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}article p a:active {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;} article p a:focus {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}';
+    var css3='.link-collection {background: #5F8BD6;transition:all .8s ease-in-out;width:100%;max-height:60px;overflow:scroll;position: fixed;right: 0;bottom: 0;text-align: center;padding: 10px 50px;z-index: 999;margin-bottom: 0;font-size:14px;}';
+    var css4='.link-collection h3 {margin: 10px !important;color: #333;} .link-collection:hover {max-height:2000px;background: #fff;border-top: 8px solid #333;}';
+    var allcss = css1 + css2 + css3 + css4;
+
     style=document.createElement('style');
     if (style.styleSheet) {
-      style.styleSheet.cssText=css1;
-      style.styleSheet.cssText=css2;
-      style.styleSheet.cssText=css3;
-      style.styleSheet.cssText=css4;
+      style.styleSheet.cssText=allcss;
     }
     else {
-      style.appendChild(document.createTextNode(css1));
-      style.appendChild(document.createTextNode(css2));
-      style.appendChild(document.createTextNode(css3));
-      style.appendChild(document.createTextNode(css4));
+      style.appendChild(document.createTextNode(allcss));
     }
     document.getElementsByTagName('head')[0].appendChild(style);
 
