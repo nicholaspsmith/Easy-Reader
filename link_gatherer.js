7b6fb77abf9d5d418065da1f93bd6fddff1ed0c2
Nick P Smith <npsmith1990@gmail.com>
2015-08-01 17:39:17 -0500

    Do not color weird blue color

--- link_gatherer.js
+++ link_gatherer.js
@@ -7,10 +7,12 @@ docReady(function(){
 
     var p = document.querySelectorAll('article p')[0];
     var pColor = getComputedStyle(p)['color'];
+    var maxHeight = window.innerHeight;
+    maxHeight /= 2;
     var css1='article p a{color:' + pColor + ';}';
     var css2='article p a:hover {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}article p a:active {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;} article p a:focus {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}';
-    var css3='.link-collection {background: #5F8BD6;transition:all .8s ease-in-out;width:100%;max-height:60px;overflow:scroll;position: fixed;right: 0;bottom: 0;text-align: center;padding: 10px 50px;z-index: 999;margin-bottom: 0;font-size:14px;}';
-    var css4='.link-collection h3 {margin: 10px !important;color: #333;} .link-collection:hover {max-height:2000px;background: #fff;border-top: 8px solid #333;}';
+    var css3='.link-collection {border-top: 4px solid #333;background: #fff;transition:all .8s ease-in-out;width:100%;max-height:60px;overflow:scroll;position: fixed;right: 0;bottom: 0;text-align: center;padding: 10px 50px;z-index: 999;margin-bottom: 0;font-size:14px;}';
+    var css4='.link-collection h3 {margin: 10px !important;color: #333;} .link-collection:hover {max-height:' + maxHeight + 'px;background: #fff;border-top: 8px solid #333;}';
     var allcss = css1 + css2 + css3 + css4;
 
     style=document.createElement('style');
