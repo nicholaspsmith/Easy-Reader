f49b906c27751f5f2daca4554eb5ccf2b4cb6f7a
Nick P Smith <npsmith1990@gmail.com>
2015-08-03 20:23:30 -0500

    add some checks for undefined to prevent console errors on non-news sites

--- link_gatherer.js
+++ link_gatherer.js
@@ -1,30 +1,38 @@
 docReady(function(){
-    document.getElementsByTagName('article')[0].innerHTML += '<div class="link-collection"><h3>Collected Links</h3></div>';
-    var lc = document.getElementsByClassName('link-collection')[0];
+  var article = document.getElementsByTagName('article');
+  if (article) {
+    article[0].innerHTML += '<div class="link-collection"><h3>Collected Links</h3></div>';
+  }
+  var lc = document.getElementsByClassName('link-collection');
+  if (lc) {
+    lc = lc[0];
     lc.style.textAlign = 'center';
     lc.style.margin = '30px 0 0';
-
-
-    var p = document.querySelectorAll('article p')[0];
-    var pColor = getComputedStyle(p)['color'];
-    var maxHeight = window.innerHeight;
-    maxHeight /= 2;
-    var css1='article p a{color:' + pColor + ';}';
-    var css2='article p a:hover {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}article p a:active {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;} article p a:focus {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}';
-    var css3='.link-collection {border-top: 4px solid #333;background: #fff;transition:all .8s ease-in-out;width:100%;max-height:60px;overflow:scroll;position: fixed;right: 0;bottom: 0;text-align: center;padding: 10px 50px;z-index: 999;margin-bottom: 0;font-size:14px;}';
-    var css4='.link-collection h3 {margin: 10px !important;color: #333;} .link-collection:hover {max-height:' + maxHeight + 'px;background: #fff;border-top: 8px solid #333;}';
-    var allcss = css1 + css2 + css3 + css4;
-
-    style=document.createElement('style');
-    if (style.styleSheet) {
-      style.styleSheet.cssText=allcss;
+  }
+  var p = document.querySelectorAll('article p');
+  if (p) {
+    p = p[0];
+    if (typeof p !== 'undefined') {
+      var pColor = getComputedStyle(p)['color'];
     }
-    else {
-      style.appendChild(document.createTextNode(allcss));
-    }
-    document.getElementsByTagName('head')[0].appendChild(style);
-
-    var ars = document.querySelectorAll('article p a');
+  }
+  var maxHeight = window.innerHeight;
+  maxHeight /= 2;
+  var css1='article p a{color:' + pColor + ';}';
+  var css2='article p a:hover {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}article p a:active {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;} article p a:focus {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}';
+  var css3='.link-collection {border-top: 4px solid #333;background: #fff;transition:all .8s ease-in-out;width:100%;max-height:60px;overflow:scroll;position: fixed;right: 0;bottom: 0;text-align: center;padding: 10px 50px;z-index: 999;margin-bottom: 0;font-size:14px;}';
+  var css4='.link-collection h3 {margin: 10px !important;color: #333;} .link-collection:hover {max-height:' + maxHeight + 'px;background: #fff;border-top: 8px solid #333;}';
+  var allcss = css1 + css2 + css3 + css4;
+  style=document.createElement('style');
+  if (style.styleSheet) {
+    style.styleSheet.cssText=allcss;
+  }
+  else {
+    style.appendChild(document.createTextNode(allcss));
+  }
+  document.getElementsByTagName('head')[0].appendChild(style);
+  var ars = document.querySelectorAll('article p a');
+  if (typeof ars !== 'undefined') {
     for (var i = 0; i < ars.length; i++) {
       ars[i].style.border = 'none';
       ars[i].style.boxShadow = 'none';
@@ -36,5 +44,5 @@ docReady(function(){
         e.preventDefault();
       });
     }
-
+  }
 });
