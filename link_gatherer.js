b444a75ba2440cbf0223bacf6591d653f0c9fbef
Nick P Smith <npsmith1990@gmail.com>
2015-08-11 11:26:17 -0500

    More error checking

--- link_gatherer.js
+++ link_gatherer.js
@@ -1,10 +1,10 @@
 docReady(function(){
   var article = document.getElementsByTagName('article');
-  if (typeof article !== 'undefined') {
+  if (typeof article !== 'undefined' && typeof article[0] !== 'undefined') {
     article[0].innerHTML += '<div class="link-collection"><h3>Collected Links</h3></div>';
   }
   var lc = document.getElementsByClassName('link-collection');
-  if (typeof lc !== 'undefined') {
+  if (typeof lc !== 'undefined' && typeof lc[0] !== 'undefined') {
     lc = lc[0];
     lc.style.textAlign = 'center';
     lc.style.margin = '30px 0 0';
@@ -18,7 +18,7 @@ docReady(function(){
   }
   var maxHeight = window.innerHeight;
   maxHeight /= 2;
-  var css1='article p a{color:' + pColor + ';}';
+  var css1='article p a{color:' + pColor + ' !important;}';
   var css2='article p a:hover {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}article p a:active {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;} article p a:focus {text-decoration:none !important;color:' + pColor + ' !important;background: none !important;opacity:1 !important;cursor:text;}';
   var css3='.link-collection {border-top: 4px solid #333;background: #fff;transition:all .8s ease-in-out;width:100%;max-height:60px;overflow:scroll;position: fixed;right: 0;bottom: 0;text-align: center;padding: 10px 50px;z-index: 999;margin-bottom: 0;font-size:14px;}';
   var css4='.link-collection h3 {margin: 10px !important;color: #333;} .link-collection:hover {max-height:' + maxHeight + 'px;background: #fff;border-top: 8px solid #333;}';
